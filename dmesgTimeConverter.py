c9955dea95f225ff2791272200d5b024597b345a
Lisitskyi Oleksii <ffolax@gmail.com>
2020-10-23 14:58:04 +0300

    finished

--- dmesgTimeConverter.py
+++ dmesgTimeConverter.py
@@ -1,40 +1,53 @@
 #!/usr/bin/env python3
-from datetime import datetime
+import datetime
 import subprocess
 import sys
 
-# Get seconds from system uptime file;
-cmd_res = subprocess.check_output(['cut', '-d', ' ', '-f', '1', '/proc/uptime'])
-kernel_started_sec = cmd_res.decode("utf-8")
-
 # Check input arguments;
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        print("Script starting! Kernel started %s seconds ago." % (kernel_started_sec))
+    if len(sys.argv) == 3:
+        print("Script starting! System started at %s" % (sys.argv[2]))
         path_to_file = sys.argv[1]
         print("Path to file: %s" % (path_to_file))
     else:
         print("Error. Invalid input parameters.")
         sys.exit(1)
 
-# Open file;
-file = open(sys.argv[1], 'r')
-Lines = file.readlines()
+# Convert string to date; 22-10-2020_08:11:21
+_data       =   sys.argv[2]
+_day        =   int(_data[0:2])
+_month      =   int(_data[3:5])
+_year       =   int(_data[6:10])
+_hours      =   int(_data[11:13])
+_minutes    =   int(_data[14:16])
+_seconds    =   int(_data[17:19])
 
-for line in Lines:
+# Set date object;
+uptime_datetime = datetime.datetime(_year, _month, _day, _hours, _minutes,_seconds)
+
+# Open file for read;
+file = open(sys.argv[1], 'r')
+lines = file.readlines()
+
+# Open file for write;
+outFile = open(path_to_file + ".edited", 'w')
+
+for line in lines:
     # Get positions of seconds;
     pos_1 = line.find("[")
     pos_2 = line.find(']')
     # Get seconds;
-    process_started_sec = line[line.find('[')+1 : line.find(']')].strip()
-    _date = round(float(kernel_started_sec), 2) - round(float(process_started_sec), 2)
-    #print(_date)
-    cmd = subprocess.check_output(['date', '-d', '%f seconds ago' % (_date)])
-    print(cmd.decode("utf-8"), end=' ')
+    process_started_sec = round(float(line[line.find('[')+1 : line.find(']')].strip()), 6)
+    # Subtract seconds from datetime;
+    process_started_date = uptime_datetime - datetime.timedelta(seconds=process_started_sec)
+    result = str((process_started_date.strftime("%Y/%m/%d %H:%M:%S")))
+    
+    # Edit file time in seconds to date;
+    new_line = "[" + result + "] " + line[pos_2+1:]
+    print(new_line, end="")
 
-    #date -d "$(</proc/uptime awk '{print $1}') seconds ago"
+    # Write to file;
+    outFile.write(new_line)
 
-    # Get line without seconds;
-    #line_without_sec = line[pos_2+1:]
-    #print(line_without_sec)
+outFile.close()
 
